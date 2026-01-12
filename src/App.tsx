7569073d6bc8836f1be70acee23febd986b388ee
Lokesh Karki <LKSHKARKI@GMAIL.COM>
2026-01-13 02:55:22 +0530

    feat: Refactor app structure with routing; add main layout and views for home and jobs and also for notFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import HomePage from './views/HomePage'
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './views/JobsPage'
+import NotFoundPage from './views/NotFoundPage'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path ='/' element ={<MainLayout />}>
+    <Route index element = {<HomePage/>}/>
+    <Route path='/jobs' element = {<JobsPage/>}/>
+    <Route path='*' element = {<NotFoundPage/>}/>
+  </Route>
+)
+)
 
 
 const App = () => {
@@ -12,26 +21,7 @@ const App = () => {
     color: 'red',
     fontSize: '55px',
   }
-  return (
-    <>
-      <Navbar/>
-      <Hero/>
-      <HomeCards/>
-      <JobListings/>
-      <ViewAllJobs/>
-
-    {/* <!-- Hero --> */}
-  
-
-    {/* <!-- Developers and Employers --> */}
-    
-
-    {/* <!-- Browse Jobs --> */}
-    
-   
-
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
